1c933a3597b6c48c88c6729651bc316d4933c76b
smanikantam <sadammanikantam@gmail.com>
2022-09-05 04:59:01 +0530

    reshaping mrtx linked lst

--- sqrlinkedlist.c
+++ sqrlinkedlist.c
@@ -4,9 +4,10 @@ struct node{
 	int value;
 	struct node *lf,*ld,*lb;
 }*head=NULL,*current=NULL,*headf=NULL;
-int count=0,rem;
+int count=0,rem,lim=2,matlim,sum_bit;
 void insert(int a);
 void display();
+struct node *reshape(struct node*);
 int main(){
 	int ch,x;
 	printf("choose\n");
@@ -26,7 +27,6 @@ int main(){
 }
 void insert(int a){
 	struct node *newnode;
-	rem=count%3;
 	newnode=(struct node *) malloc(sizeof(struct node));
 	newnode->value=a;
 	if(head==NULL){
@@ -34,33 +34,92 @@ void insert(int a){
 		head=newnode;
 		current=newnode;
 	}
+	else if(count<2){
+		current->lf=newnode;
+		current=newnode;
+	}
 	else{
-		if(count<3){
-			current->lf=newnode;
-			current=newnode;
+		sum_bit=count&(count-1);
+		matlim=lim*lim;
+		if(count>=matlim && sum_bit==0){
+			lim+=1;
+			matlim=lim*lim;
+			// rem=count%lim;
+			current=reshape(newnode);
 		}
-		else if(rem==0){
-			if((count/3)>=2){
+		else{
+			int temp_rem;
+			struct node *temp_head;
+			temp_head=head;
+			rem=count%lim;
+			if((count/lim)>=2 && rem==0){
 				head=head->lb;
+				temp_head=head;
 			}
-			current->lf=newnode;
-			head->lb=newnode;
-			current=newnode;
-		}
-		else if(rem==1){
-			current->lf=newnode;
-			head->ld=newnode;
-			head->lf->lb=newnode;
-			current=newnode;
-		}
-		else if(rem==2){
-			current->lf=newnode;
-			head->lf->ld=newnode;
-			current=newnode;
+			else if(rem>0){
+				for(temp_rem=rem-1;temp_rem!=0;temp_rem--){
+					temp_head=temp_head->lf;
+				}
+				if(rem==1 && count>lim){
+					current->lf=newnode;
+					head->ld=newnode;
+					head->lf->lb=newnode;
+					current=newnode;
+				}
+				else{
+					current->lf=newnode;
+					temp_head->ld=newnode;
+					temp_head->lf->lb=newnode;
+					current=newnode;
+				}
+			}
 		}
 	}
-	count+=1;
+	count++;
 }
+struct node *reshape(struct node *newnode){
+	struct node *temp_head=headf,*temp_headf=headf,*temp_current=headf,*pointer=headf;
+	int temp_count=1,temp_rem;
+	while(temp_current->lf!=NULL){
+		temp_rem=temp_count%lim;
+		if(temp_count<lim){
+			temp_current=pointer;
+		}
+		else if(temp_rem==0){
+			if((temp_count/lim)>=2){
+				temp_head=temp_head->lb;
+			}
+			temp_head->lb=pointer;
+			temp_current->lf=pointer;
+		}
+		else{
+			for(temp_rem=rem-1;temp_rem!=0;temp_rem--){
+				temp_head=temp_head->lf;
+			}
+			if(temp_rem==1 && temp_count>lim){
+				temp_current->lf=pointer;
+				temp_head->ld=pointer;
+				temp_head->lf->lb=pointer;
+			}
+			else if(rem==lim-1){
+				temp_current->lf=pointer;
+				temp_head->lf->ld=pointer;
+			}
+			else{
+				temp_current->lf=pointer;
+				temp_head->ld=pointer;
+				temp_head->lf->lb=pointer;
+			}
+		}
+		pointer=pointer->lf;
+		temp_current=pointer;
+		temp_count++;
+	}
+	temp_current->lf=newnode;
+	temp_current=newnode;
+	return temp_current;
+}
+
 void display(){
 	int ch;
 	printf("1.display\t2.diagonal_display\n");
@@ -82,7 +141,6 @@ void display(){
 				break;
 	}
 }
-
 
 
 
